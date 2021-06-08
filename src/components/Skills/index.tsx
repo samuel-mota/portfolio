40d55a01e020ec7675df0e1f136d9e53b6ce4711
Samuel Mota <smagnun@gmail.com>
2021-06-08 18:56:12 +0200

    skills and tools icons done

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -7,34 +7,34 @@ const Skills = () => {
       <Intro title="skills" />
       <div className={styles.icons}>
         <div className={styles.icon}>
-          <img src="/assets/icons/node-icon.svg" alt="node icon" />
+          <img
+            src="/assets/icons/skills-icons/js-icon.svg"
+            alt="javascript icon"
+          />
         </div>
         <div className={styles.icon}>
-          <img src="/assets/icons/node-icon.svg" alt="node icon" />
+          <img
+            src="/assets/icons/skills-icons/ts-icon.svg"
+            alt="typescript icon"
+          />
         </div>
         <div className={styles.icon}>
-          <img src="/assets/icons/node-icon.svg" alt="node icon" />
+          <img src="/assets/icons/skills-icons/css-icon.svg" alt="css icon" />
         </div>
         <div className={styles.icon}>
-          <img src="/assets/icons/node-icon.svg" alt="node icon" />
+          <img src="/assets/icons/skills-icons/html-icon.svg" alt="html icon" />
         </div>
         <div className={styles.icon}>
-          <img src="/assets/icons/node-icon.svg" alt="node icon" />
+          <img src="/assets/icons/skills-icons/react-icon.svg" alt="react icon" />
         </div>
         <div className={styles.icon}>
-          <img src="/assets/icons/node-icon.svg" alt="node icon" />
+          <img src="/assets/icons/skills-icons/next-icon-light.svg" alt="next icon" />
         </div>
         <div className={styles.icon}>
-          <img src="/assets/icons/node-icon.svg" alt="node icon" />
+          <img src="/assets/icons/skills-icons/sass-icon.svg" alt="sass icon" />
         </div>
         <div className={styles.icon}>
-          <img src="/assets/icons/node-icon.svg" alt="node icon" />
-        </div>
-        <div className={styles.icon}>
-          <img src="/assets/icons/node-icon.svg" alt="node icon" />
-        </div>
-        <div className={styles.icon}>
-          <img src="/assets/icons/node-icon.svg" alt="node icon" />
+          <img src="/assets/icons/skills-icons/bootstrap-icon.svg" alt="bootstrap icon" />
         </div>
       </div>
     </div>
--- src/components/Tools/index.tsx
+++ src/components/Tools/index.tsx
@@ -7,34 +7,31 @@ const Tools = () => {
       <Intro title="tools" />
       <div className={styles.icons}>
         <div className={styles.icon}>
-          <img src="/assets/icons/node-icon.svg" alt="node icon" />
+          <img
+            src="/assets/icons/tools-icons/atlassian-icon.svg"
+            alt="atlassian icon"
+          />
         </div>
         <div className={styles.icon}>
-          <img src="/assets/icons/node-icon.svg" alt="node icon" />
+          <img src="/assets/icons/tools-icons/heroku-icon-light.svg" alt="heroku icon" />
         </div>
         <div className={styles.icon}>
-          <img src="/assets/icons/node-icon.svg" alt="node icon" />
+          <img src="/assets/icons/tools-icons/jira-icon.svg" alt="jira icon" />
         </div>
         <div className={styles.icon}>
-          <img src="/assets/icons/node-icon.svg" alt="node icon" />
+          <img src="/assets/icons/tools-icons/netlify-icon.svg" alt="netlify icon" />
         </div>
         <div className={styles.icon}>
-          <img src="/assets/icons/node-icon.svg" alt="node icon" />
+          <img src="/assets/icons/tools-icons/ps-icon-light.svg" alt="ps icon" />
         </div>
         <div className={styles.icon}>
-          <img src="/assets/icons/node-icon.svg" alt="node icon" />
+          <img src="/assets/icons/tools-icons/trello-icon.svg" alt="trello icon" />
         </div>
         <div className={styles.icon}>
-          <img src="/assets/icons/node-icon.svg" alt="node icon" />
+          <img src="/assets/icons/tools-icons/vercel-icon-light.svg" alt="vercel icon" />
         </div>
         <div className={styles.icon}>
-          <img src="/assets/icons/node-icon.svg" alt="node icon" />
-        </div>
-        <div className={styles.icon}>
-          <img src="/assets/icons/node-icon.svg" alt="node icon" />
-        </div>
-        <div className={styles.icon}>
-          <img src="/assets/icons/node-icon.svg" alt="node icon" />
+          <img src="/assets/icons/tools-icons/vscode-icon.svg" alt="vscode icon" />
         </div>
       </div>
     </div>
